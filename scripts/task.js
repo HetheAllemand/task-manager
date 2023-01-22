ea3fb917637c546fd71c687a4ed8ada2af3dacd2
HetheAllemand <118325715+HetheAllemand@users.noreply.github.com>
2023-01-22 14:42:11 -0500

    class 3

---
+++ scripts/task.js
@@ -0,0 +1,18 @@
+
+class Task {
+
+    constructor(title, description, dueDate, category, contact, status, important){
+        this.title = title;
+        this.description = description;
+        this.dueDate = dueDate;
+        this.category = category;
+        this.contact = contact;
+        this.status = status;
+        this.important = important;
+
+        this.name = "Hethe";
+    }
+}
+
+
+
